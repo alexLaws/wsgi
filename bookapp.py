6fc574d9dd44dd4ba433b8598c4d4b7cbf8ac598
Alex Laws <tal1286@gmail.com>
2018-11-07 17:49:07 -0800

    books database assignment

--- bookapp.py
+++ bookapp.py
@@ -1,4 +1,5 @@
 import re
+import traceback
 
 from bookdb import BookDB
 
@@ -6,18 +7,66 @@ DB = BookDB()
 
 
 def book(book_id):
-    return "<h1>a book with id %s</h1>" % book_id
+    book = DB.title_info(book_id)
+    try:
+        book_info = ['<h1>{}</h1>'.format(book['title']), '<table>']
+        book_info.append('<tr><th>Author</th><td>{}</td></tr>'.format(book['author']))
+        book_info.append('<tr><th>Publisher</th><td>{}</td></tr>'.format(book['publisher']))
+        book_info.append('<tr><th>ISBN</th><td>{}</td></tr>'.format(book['isbn']))
+    except TypeError:
+        raise NameError
+
+    return '\n'.join(book_info)
 
 
 def books():
-    return "<h1>a list of books</h1>"
+    book_list = ['<h1>Here is a list of books!</h1>\n', '<ul>']
+
+    for book in DB.titles():
+        book_list.append('<li><a href="/book/{}">{}</a></li>'.format(book['id'],
+                                                                     book['title']))
+    book_list.append('</ul>')
+
+    return '\n'.join(book_list)
 
 
 def application(environ, start_response):
-    status = "200 OK"
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    return ["<h1>No Progress Yet</h1>".encode('utf8')]
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
+
+def resolve_path(path):
+    funcs = {'': books,
+             'book': book}
+
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
+
+    return func, args
 
 
 if __name__ == '__main__':
